2553e5346fae64aeb6e527eb43c93be993dc2579
Yoochanhong <chanhongy6@naver.com>
2022-11-04 18:49:40 +0900

    ⚡️ :: 팔로잉 목록 가져오기 성공

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,47 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
+
+
+class Follow {
+  String? name;
+
+  Follow({this.name});
+
+  Follow.fromJson(Map<String, dynamic> json) {
+    name = json['login'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['login'] = this.name;
+    return data;
+  }
+}
+
+class FollowList{
+  final List<Follow>? follow;
+  FollowList({this.follow});
+  factory FollowList.fromJson(List<dynamic> json){
+    List<Follow> follow= <Follow>[];
+    follow = json.map((i) => Follow.fromJson(i)).toList();
+
+    return FollowList(
+      follow: follow,
+    );
+  }
+}
+
+Future<FollowList> getFollowApi() async {
+  final response = await http
+      .get(Uri.parse('https://api.github.com/users/Yoochanhong/following'));
+  print(response.body);
+  if (response.statusCode == 200) {
+    return FollowList.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('예외');
+  }
+}
 
 void main() => runApp(MyApp());
 
@@ -12,34 +53,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Future<FollowList>? follow;
+
+  @override
+  void initState() {
+    super.initState();
+    follow = getFollowApi();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class follow {
-  String? name;
-
-  follow({this.name});
-
-  follow.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['name'] = this.name;
-    return data;
-  }
-}
-
-Future<follow> getFollowApi() async {
-  final response = await http.get(Uri.parse('https://api.github.com/users/Yoochanhong/following'));
-  print(response.body);
-  if (response.statusCode == 200){
-    return follow.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('예외');
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FutureBuilder(
+                  future: follow,
+                  builder: (context, snapshot) {
+                    if(snapshot.hasData){
+                      return Text('엄');
+                    } else if (snapshot.hasError){
+                      return Text('에러 엄');
+                    } else {
+                      return Text('실패');
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
