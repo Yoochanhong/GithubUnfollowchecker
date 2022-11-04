703fc13e0a54d73ebd6b598384bbcfd1e2af872e
Yoochanhong <chanhongy6@naver.com>
2022-11-04 19:59:24 +0900

    ⚡️ :: 팔로잉 목록 전부 가져오기 성공(최대 100명)

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,100 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 class Follow {
-  String? name;
+  String? login;
+  int? id;
+  String? nodeId;
+  String? avatarUrl;
+  String? gravatarId;
+  String? url;
+  String? htmlUrl;
+  String? followersUrl;
+  String? followingUrl;
+  String? gistsUrl;
+  String? starredUrl;
+  String? subscriptionsUrl;
+  String? organizationsUrl;
+  String? reposUrl;
+  String? eventsUrl;
+  String? receivedEventsUrl;
+  String? type;
+  bool? siteAdmin;
 
-  Follow({this.name});
+  Follow(
+      {this.login,
+        this.id,
+        this.nodeId,
+        this.avatarUrl,
+        this.gravatarId,
+        this.url,
+        this.htmlUrl,
+        this.followersUrl,
+        this.followingUrl,
+        this.gistsUrl,
+        this.starredUrl,
+        this.subscriptionsUrl,
+        this.organizationsUrl,
+        this.reposUrl,
+        this.eventsUrl,
+        this.receivedEventsUrl,
+        this.type,
+        this.siteAdmin});
 
   Follow.fromJson(Map<String, dynamic> json) {
-    name = json['login'];
+    login = json['login'];
+    id = json['id'];
+    nodeId = json['node_id'];
+    avatarUrl = json['avatar_url'];
+    gravatarId = json['gravatar_id'];
+    url = json['url'];
+    htmlUrl = json['html_url'];
+    followersUrl = json['followers_url'];
+    followingUrl = json['following_url'];
+    gistsUrl = json['gists_url'];
+    starredUrl = json['starred_url'];
+    subscriptionsUrl = json['subscriptions_url'];
+    organizationsUrl = json['organizations_url'];
+    reposUrl = json['repos_url'];
+    eventsUrl = json['events_url'];
+    receivedEventsUrl = json['received_events_url'];
+    type = json['type'];
+    siteAdmin = json['site_admin'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['login'] = this.name;
+    data['login'] = this.login;
+    data['id'] = this.id;
+    data['node_id'] = this.nodeId;
+    data['avatar_url'] = this.avatarUrl;
+    data['gravatar_id'] = this.gravatarId;
+    data['url'] = this.url;
+    data['html_url'] = this.htmlUrl;
+    data['followers_url'] = this.followersUrl;
+    data['following_url'] = this.followingUrl;
+    data['gists_url'] = this.gistsUrl;
+    data['starred_url'] = this.starredUrl;
+    data['subscriptions_url'] = this.subscriptionsUrl;
+    data['organizations_url'] = this.organizationsUrl;
+    data['repos_url'] = this.reposUrl;
+    data['events_url'] = this.eventsUrl;
+    data['received_events_url'] = this.receivedEventsUrl;
+    data['type'] = this.type;
+    data['site_admin'] = this.siteAdmin;
     return data;
   }
 }
 
-class FollowList{
+class FollowList {
   final List<Follow>? follow;
+
   FollowList({this.follow});
-  factory FollowList.fromJson(List<dynamic> json){
-    List<Follow> follow= <Follow>[];
+
+  factory FollowList.fromJson(List<dynamic> json) {
+    List<Follow> follow = <Follow>[];
     follow = json.map((i) => Follow.fromJson(i)).toList();
 
     return FollowList(
@@ -34,7 +105,7 @@ class FollowList{
 
 Future<FollowList> getFollowApi() async {
   final response = await http
-      .get(Uri.parse('https://api.github.com/users/Yoochanhong/following'));
+      .get(Uri.parse('https://api.github.com/users/Yoochanhong/following?per_page=100'));
   print(response.body);
   if (response.statusCode == 200) {
     return FollowList.fromJson(jsonDecode(response.body));
@@ -74,9 +145,18 @@ class _MyAppState extends State<MyApp> {
                 FutureBuilder(
                   future: follow,
                   builder: (context, snapshot) {
-                    if(snapshot.hasData){
-                      return Text('엄');
-                    } else if (snapshot.hasError){
+                    if (snapshot.hasData) {
+                      return Container(
+                        width: 300,
+                        height: 500,
+                        child: ListView.builder(
+                          itemCount: 91,
+                          itemBuilder: (context, index) {
+                            return Text(snapshot.data!.follow![index].login.toString());
+                          },
+                        ),
+                      );
+                    } else if (snapshot.hasError) {
                       return Text('에러 엄');
                     } else {
                       return Text('실패');
