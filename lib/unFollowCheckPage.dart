2a14763138a98b0f639eb96bd1b75dc6a9c4c8d3
Yoochanhong <chanhongy6@naver.com>
2022-11-07 10:39:23 +0900

    unFollowCheckPage 생성

---
+++ lib/unFollowCheckPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UnFollowCheckpage extends StatefulWidget {
+  const UnFollowCheckpage({Key? key}) : super(key: key);
+
+  @override
+  State<UnFollowCheckpage> createState() => _UnFollowCheckpageState();
+}
+
+class _UnFollowCheckpageState extends State<UnFollowCheckpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
