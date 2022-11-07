1e300d48d214c703e8e6368b0bab4a06291fb21e
Yoochanhong <chanhongy6@naver.com>
2022-11-07 11:34:05 +0900

    최초로 보일 화면 로그인페이지로 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,17 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:github_unfollow_checker/model.dart';
-import 'package:github_unfollow_checker/token.dart';
-import 'package:http/http.dart' as http;
+import 'package:github_unfollow_checker/loginPage.dart';
 
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-
-  @override
-  void initState() {
-    super.initState();
-    follow = getFollowApi();
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:
+      home: LoginPage(),
     );
   }
 }
