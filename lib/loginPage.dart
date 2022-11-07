960f1bbf7f76a71f86ed08feb21cffc4a677e74a
Yoochanhong <chanhongy6@naver.com>
2022-11-07 12:45:43 +0900

    간단하게 UI 구성

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -5,6 +5,35 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    TextEditingController controller = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('gitHubUnFollowChecker'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: TextField(
+                  controller: controller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: '깃허브 아이디를 입력해주세요.'
+                  ),
+                  keyboardType: TextInputType.name,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('버튼'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
