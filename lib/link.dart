47ca8df611c3dcee7cfeaecd71857461d7bda299
Yoochanhong <chanhongy6@naver.com>
2022-11-09 18:30:26 +0900

    삭제

--- lib/link.dart
+++
@@ -1,9 +0,0 @@
-import 'package:url_launcher/url_launcher.dart';
-
-launchURL(String link) async {
-  if (await canLaunchUrl(Uri.parse(link))) {
-    await launchUrl(Uri.parse(link));
-  } else {
-    throw 'Could not launch $link';
-  }
-}
