70280275f4606dc7a43a2e5693d027e20a47b607
Yoochanhong <chanhongy6@naver.com>
2022-11-10 21:55:30 +0900

    키보드 자동 숨김 오류 해결

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -10,59 +10,54 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formkey = GlobalKey<FormState>();
+  TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController controller = TextEditingController();
-    //final formkey = GlobalKey<FormState>();
     return Scaffold(
       backgroundColor: Color(0xffFFFFFF),
-      body: Container(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 80, bottom: 65),
-              child: Image.asset(
-                'assets/app-logo.png',
-                width: 150,
-              ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 80, bottom: 65),
+            child: Image.asset(
+              'assets/app-logo.png',
+              width: 150,
             ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                child: Form(
-                  //key: formkey,
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value!.isEmpty) return '아이디를 입력해주세요.';
-                      return null;
-                    },
-                    controller: controller,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: '깃허브 아이디를 입력해주세요.'),
-                    keyboardType: TextInputType.name,
-                    onFieldSubmitted: (String str) {
-                      //if (formkey.currentState!.validate()) {
-                        Get.to(UnFollowCheckpage(), arguments: str);
-                        print(str);
-                      //}
-                    },
-                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Form(
+                key: _formkey,
+                child: TextFormField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: '깃허브 아이디를 입력해주세요.'),
+                  keyboardType: TextInputType.name,
+                  validator: (value) =>
+                  value!.isEmpty ? '아이디를 입력해주세요.' : null,
+                  onFieldSubmitted: (String str) {
+                    if (_formkey.currentState!.validate()) {
+                    Get.to(UnFollowCheckpage(), arguments: str);
+                    }
+                  },
                 ),
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                //if (formkey.currentState!.validate()) {
-                  Get.to(UnFollowCheckpage(), arguments: controller.text);
-                  print(controller.text);
-                //}
-              },
-              child: Icon(Icons.search),
-            ),
-          ],
-        ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_formkey.currentState!.validate()) {
+                Get.to(UnFollowCheckpage(), arguments: controller.text);
+              }
+            },
+            child: const Icon(Icons.search),
+          ),
+        ],
       ),
     );
   }
