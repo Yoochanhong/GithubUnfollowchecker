95251087e39bd07748a20800e5c29b78eb1ef586
맥북 유찬홍 <chanhongy6@naver.com>
2023-02-06 14:38:20 +0900

    stf 위젯으로 변경

--- lib/View/web_view.dart
+++ lib/View/web_view.dart
@@ -2,17 +2,19 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+class WebViewScreen extends StatefulWidget {
+  WebViewScreen({Key? key}) : super(key: key);
 
-class WebViewScreen extends StatelessWidget {
-  const WebViewScreen({Key? key}) : super(key: key);
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
 
+class _WebViewScreenState extends State<WebViewScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: WebView(
-          initialUrl: Get.arguments,
-        ),
+        body: WebViewWidget(controller: controller),
       ),
     );
   }
