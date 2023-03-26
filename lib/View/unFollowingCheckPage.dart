fe3aa77770fe16b05b0934c8f6d910624118ed80
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-26 17:19:38 +0900

    필요없는 SizedBox 제거

--- lib/View/unFollowingCheckPage.dart
+++ lib/View/unFollowingCheckPage.dart
@@ -26,67 +26,65 @@ class _UnFollowingCheckpageState extends State<UnFollowingCheckpage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffFFFFFF),
-      body: SizedBox(
-        child: Center(
-          child: FutureBuilder(
-            future: follower,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return SizedBox(
-                  width: 300,
-                  height: 500,
-                  child: ListView.builder(
-                    itemCount: snapshot.data!.follow!.length,
-                    itemBuilder: (context, index) {
-                      unfollowing = getUnfollowingApi(
-                          snapshot.data!.follow![index].login.toString());
-                      return FutureBuilder(
-                        future: unfollowing,
-                        builder: (context, snapshot1) {
-                          if (snapshot1.hasData) {
-                            return ListTile(
-                              leading: Image.network(snapshot
-                                  .data!.follow![index].avatarUrl
-                                  .toString()),
-                              title: Text(snapshot1.data.toString()),
-                              onLongPress: () {
-                                String url = snapshot
-                                    .data!.follow![index].htmlUrl
-                                    .toString();
-                                Get.to(WebViewScreen(), arguments: url);
-                              },
-                            );
-                          } else {
-                            return const SizedBox.shrink();
-                          }
-                        },
-                      );
-                    },
+      body: Center(
+        child: FutureBuilder(
+          future: follower,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return SizedBox(
+                width: 300,
+                height: 500,
+                child: ListView.builder(
+                  itemCount: snapshot.data!.follow!.length,
+                  itemBuilder: (context, index) {
+                    unfollowing = getUnfollowingApi(
+                        snapshot.data!.follow![index].login.toString());
+                    return FutureBuilder(
+                      future: unfollowing,
+                      builder: (context, snapshot1) {
+                        if (snapshot1.hasData) {
+                          return ListTile(
+                            leading: Image.network(snapshot
+                                .data!.follow![index].avatarUrl
+                                .toString()),
+                            title: Text(snapshot1.data.toString()),
+                            onLongPress: () {
+                              String url = snapshot
+                                  .data!.follow![index].htmlUrl
+                                  .toString();
+                              Get.to(WebViewScreen(), arguments: url);
+                            },
+                          );
+                        } else {
+                          return const SizedBox.shrink();
+                        }
+                      },
+                    );
+                  },
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 80, bottom: 65),
+                    child: Image.asset(
+                      'assets/noPerson.png',
+                      width: 200,
+                    ),
                   ),
-                );
-              } else if (snapshot.hasError) {
-                return Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 80, bottom: 65),
-                      child: Image.asset(
-                        'assets/noPerson.png',
-                        width: 200,
-                      ),
-                    ),
-                    const Text(
-                      '존재하지 않는 유저입니다.',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ],
-                );
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            },
-          ),
+                  const Text(
+                    '존재하지 않는 유저입니다.',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
       ),
     );
