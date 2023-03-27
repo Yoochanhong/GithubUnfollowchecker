26d63cd0961d167ab97945d8ce2f069bc74cf785
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-27 14:17:24 +0900

    int형으로 팔로잉 구별

--- lib/ViewModel/get_unfollow_api.dart
+++ lib/ViewModel/get_unfollow_api.dart
@@ -1,15 +1,13 @@
 import 'package:http/http.dart' as http;
 import 'package:github_unfollow_checker/token.dart';
 
-Future<String> getUnfollowApi(String login, String userName) async {
+Future<int> getUnfollowApi(String login, String userName) async {
   final response = await http.get(
       Uri.parse('https://api.github.com/users/$login/following/$userName'),
       headers: {'Authorization': 'Bearer $yourToken'});
-  print(response.body);
   if (response.statusCode == 404) {
-    print(login);
-    return login;
+    return 1;
   } else {
-    throw Exception('요청 한도초과');
+    return 0;
   }
 }
