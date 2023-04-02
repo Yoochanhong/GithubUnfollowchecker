8e260782b2d57d9cecc9b53e10457b694374c4b5
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-02 20:49:55 +0900

    다시 롤백;

--- lib/ViewModel/get_unfollow_api.dart
+++ lib/ViewModel/get_unfollow_api.dart
@@ -1,13 +1,13 @@
 import 'package:http/http.dart' as http;
 import 'package:github_unfollow_checker/token.dart';
 
-Future<int> getUnfollowApi(String login, String userName) async {
+Future<String> getUnfollowApi(String login, String userName) async {
   final response = await http.get(
       Uri.parse('https://api.github.com/users/$login/following/$userName'),
       headers: {'Authorization': 'Bearer $yourToken'});
   if (response.statusCode == 404) {
-    return 1;
+    return login;
   } else {
-    return 0;
+    throw Exception("팔로잉중");
   }
 }
