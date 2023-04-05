69896f17198dbd97ea93f98c809260c08b8a0b93
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-05 15:19:11 +0900

    파베 관련 코드 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:github_unfollow_checker/View/loginPage.dart';
 import 'package:get/get.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
