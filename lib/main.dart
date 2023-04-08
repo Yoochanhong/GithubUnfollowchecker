0d7a687924bdf5e26fb5e7dffe90c59b151a68f1
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-08 18:16:20 +0900

    const 키워드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:github_unfollow_checker/View/loginPage.dart';
 import 'package:get/get.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
