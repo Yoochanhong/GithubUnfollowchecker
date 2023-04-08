bfabc6a8dbe7e9757b7b879d1b208c847219bc31
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-08 18:16:36 +0900

    파일명 변경

--- lib/ViewModel/get_unfollowers.dart
+++ lib/ViewModel/get_unfollowers.dart
@@ -4,7 +4,7 @@ import 'package:github_unfollow_checker/Model/user_list.dart';
 
 Future<UserList> getUnfollowerApi(String userName) async {
   final response = await http
-      .get(Uri.parse('http://localhost:8080/unfollower?userName=$userName'));
+      .get(Uri.parse('http://localhost:8080/unfollowers?userName=$userName'));
   if (response.statusCode == 200) {
     return UserList.fromJson(jsonDecode(response.body));
   } else {
