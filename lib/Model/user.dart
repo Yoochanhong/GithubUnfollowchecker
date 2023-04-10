cc6a3f55e7e6d787d398f81d7a7ca92a01e06750
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-10 22:12:53 +0900

    필요없는 모델 필드 제거

--- lib/Model/user.dart
+++ lib/Model/user.dart
@@ -1,84 +1,21 @@
 class User {
   String? login;
-  int? id;
-  String? nodeId;
   String? avatarUrl;
-  String? gravatarId;
-  String? url;
   String? htmlUrl;
-  String? followersUrl;
-  String? followingUrl;
-  String? gistsUrl;
-  String? starredUrl;
-  String? subscriptionsUrl;
-  String? organizationsUrl;
-  String? reposUrl;
-  String? eventsUrl;
-  String? receivedEventsUrl;
-  String? type;
-  bool? siteAdmin;
 
-  User(
-      {this.login,
-      this.id,
-      this.nodeId,
-      this.avatarUrl,
-      this.gravatarId,
-      this.url,
-      this.htmlUrl,
-      this.followersUrl,
-      this.followingUrl,
-      this.gistsUrl,
-      this.starredUrl,
-      this.subscriptionsUrl,
-      this.organizationsUrl,
-      this.reposUrl,
-      this.eventsUrl,
-      this.receivedEventsUrl,
-      this.type,
-      this.siteAdmin});
+  User({this.login, this.avatarUrl, this.htmlUrl});
 
   User.fromJson(Map<String, dynamic> json) {
     login = json['login'];
-    id = json['id'];
-    nodeId = json['node_id'];
     avatarUrl = json['avatar_url'];
-    gravatarId = json['gravatar_id'];
-    url = json['url'];
     htmlUrl = json['html_url'];
-    followersUrl = json['followers_url'];
-    followingUrl = json['following_url'];
-    gistsUrl = json['gists_url'];
-    starredUrl = json['starred_url'];
-    subscriptionsUrl = json['subscriptions_url'];
-    organizationsUrl = json['organizations_url'];
-    reposUrl = json['repos_url'];
-    eventsUrl = json['events_url'];
-    receivedEventsUrl = json['received_events_url'];
-    type = json['type'];
-    siteAdmin = json['site_admin'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['login'] = login;
-    data['id'] = id;
-    data['node_id'] = nodeId;
     data['avatar_url'] = avatarUrl;
-    data['gravatar_id'] = gravatarId;
-    data['url'] = url;
     data['html_url'] = htmlUrl;
-    data['followers_url'] = followersUrl;
-    data['following_url'] = followingUrl;
-    data['gists_url'] = gistsUrl;
-    data['starred_url'] = starredUrl;
-    data['subscriptions_url'] = subscriptionsUrl;
-    data['organizations_url'] = organizationsUrl;
-    data['repos_url'] = reposUrl;
-    data['events_url'] = eventsUrl;
-    data['received_events_url'] = receivedEventsUrl;
-    data['type'] = type;
-    data['site_admin'] = siteAdmin;
     return data;
   }
 }
