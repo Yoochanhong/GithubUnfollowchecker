9a6d30d083d233b05f105c7f9d32a0eb5a71c6f2
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-10 22:04:18 +0900

    쿼리문을 인자로 받아서 함수 중복 없애기

--- lib/ViewModel/get_unfollow.dart
+++ lib/ViewModel/get_unfollow.dart
@@ -2,9 +2,9 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:github_unfollow_checker/Model/user_list.dart';
 
-Future<UserList> getUnfollowerApi(String userName) async {
+Future<UserList> getUnfollow(String userName, String queryName) async {
   final response = await http
-      .get(Uri.parse('http://localhost:8080/unfollowers?userName=$userName'));
+      .get(Uri.parse("http://localhost:8080/$queryName?userName=$userName"));
   if (response.statusCode == 200) {
     return UserList.fromJson(jsonDecode(response.body));
   } else {
