82b7a6b0a4c356a01bb90ca785845c829b2691d1
yoochanhong <chanhongy6@naver.com>
2023-04-19 11:09:49 +0900

    유저 검색 기능 추가

--- lib/View/un_followers_check_page.dart
+++ lib/View/un_followers_check_page.dart
@@ -16,15 +16,21 @@ class UnFollowCheckpage extends StatefulWidget {
 class _UnFollowCheckpageState extends State<UnFollowCheckpage> {
   Future<UserList>? unfollower;
   List<User> list = List.empty(growable: true);
+  List<User> searchResult = List.empty(growable: true);
 
   @override
   void initState() {
     super.initState();
-    unfollower = getUnfollow(widget.userName, "unfollowers");
+    unfollower = getUnfollow(widget.userName, "unfollowers").then((value) {
+      for (int i = 0; i < value.user!.length; i++) {
+        list.add(value.user![i]);
+      }
+      return value;
+    });
   }
 
   List<User> search(String query) {
-    List<User> result = [];
+    List<User> result = List.empty(growable: true);
     for (User user in list) {
       if (user.login!.contains(query)) {
         result.add(user);
@@ -38,7 +44,13 @@ class _UnFollowCheckpageState extends State<UnFollowCheckpage> {
     return Scaffold(
       backgroundColor: const Color(0xffFFFFFF),
       appBar: AppBar(
-        title: TextField(),
+        title: TextField(
+          onChanged: (value) {
+            setState(() {
+              searchResult = search(value);
+            });
+          },
+        ),
       ),
       body: Center(
         child: FutureBuilder(
@@ -48,14 +60,13 @@ class _UnFollowCheckpageState extends State<UnFollowCheckpage> {
               if (snapshot.data!.user![0].htmlUrl == "1")
                 return Text("얘 없음");
               else {
-                for (int i = 0; i < snapshot.data!.user!.length; i++) {
-                  list.add(snapshot.data!.user![i]);
-                }
                 return SizedBox(
                   width: MediaQuery.of(context).size.width - 50,
                   height: MediaQuery.of(context).size.height - 200,
                   child: ListView.builder(
-                    itemCount: list.length,
+                    itemCount: searchResult.isEmpty
+                        ? list.length
+                        : searchResult.length,
                     itemBuilder: (context, index) {
                       return Container(
                         margin: const EdgeInsets.only(bottom: 13.0),
@@ -66,11 +77,16 @@ class _UnFollowCheckpageState extends State<UnFollowCheckpage> {
                           ),
                         ),
                         child: ListTile(
-                          leading:
-                              Image.network(list[index].avatarUrl.toString()),
-                          title: Text(list[index].login.toString()),
+                          leading: Image.network(searchResult.isEmpty
+                              ? list[index].avatarUrl.toString()
+                              : searchResult[index].avatarUrl.toString()),
+                          title: Text(searchResult.isEmpty
+                              ? list[index].login.toString()
+                              : searchResult[index].login.toString()),
                           onLongPress: () {
-                            String url = list[index].htmlUrl.toString();
+                            String url = searchResult.isEmpty
+                                ? list[index].htmlUrl.toString()
+                                : searchResult[index].htmlUrl.toString();
                             Navigator.push(
                               context,
                               MaterialPageRoute(
