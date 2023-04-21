1ace5521c2477715cee0aac1a56a06c37c71db91
yoochanhong <chanhongy6@naver.com>
2023-04-21 12:06:40 +0900

    type 필드 추가

--- lib/Model/user.dart
+++ lib/Model/user.dart
@@ -2,13 +2,15 @@ class User {
   String? login;
   String? avatarUrl;
   String? htmlUrl;
+  String? type;
 
-  User({this.login, this.avatarUrl, this.htmlUrl});
+  User({this.login, this.avatarUrl, this.htmlUrl, this.type});
 
   User.fromJson(Map<String, dynamic> json) {
     login = json['login'];
     avatarUrl = json['avatar_url'];
     htmlUrl = json['html_url'];
+    type = json['type'];
   }
 
   Map<String, dynamic> toJson() {
@@ -16,6 +18,7 @@ class User {
     data['login'] = login;
     data['avatar_url'] = avatarUrl;
     data['html_url'] = htmlUrl;
+    data['type'] = type;
     return data;
   }
 }
