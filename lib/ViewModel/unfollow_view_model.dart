c50a6d9d6e42699ede9c2e568eaeec1fdc6ea72a
yoochanhong <chanhongy6@naver.com>
2023-04-30 00:59:21 +0900

    언팔로우 뷰모델 클래스 구현

--- lib/ViewModel/unfollow_view_model.dart
+++ lib/ViewModel/unfollow_view_model.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/foundation.dart';
+import 'package:github_unfollow_checker/Model/user_list.dart';
+import 'package:github_unfollow_checker/Repository/unfollow_repository.dart';
 
-class UnfollowViewModel with ChangeNotifier{
-  
+class UnfollowViewModel with ChangeNotifier {
+  late final UnfollowRepository _repository;
+  UserList _userList = UserList(user: List.empty(growable: true));
+  final String _queryName, _userName;
+
+  UnfollowViewModel(this._queryName, this._userName) {
+    _queryName;
+    _userName;
+    _repository = UnfollowRepository();
+    _getUserList();
+  }
+
+  UserList get userList => _userList;
+
+  Future _getUserList() async {
+    _userList = await _repository.getUserList(_queryName, _userName);
+    notifyListeners();
+  }
 }
