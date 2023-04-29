9b44e4077e205ef8bd098c180b849aa12b5ca659
yoochanhong <chanhongy6@naver.com>
2023-04-30 00:48:37 +0900

    언팔로우 레포지토리 구현

--- lib/Repository/unfollow_repository.dart
+++ lib/Repository/unfollow_repository.dart
@@ -1,3 +1,9 @@
-class UnfollowRepository{
-  
+import 'package:github_unfollow_checker/DataSource/data_source.dart';
+import 'package:github_unfollow_checker/Model/user_list.dart';
+
+class UnfollowRepository {
+  final DataSource _dataSource = DataSource();
+
+  Future<UserList> getUserList(String queryName, String userName) =>
+      _dataSource.getUserList(queryName, userName);
 }
