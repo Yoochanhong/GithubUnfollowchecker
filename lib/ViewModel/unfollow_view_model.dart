4d7d04f8109a8959b0fb002dd7a88eb5f71cd4fc
yoochanhong <chanhongy6@naver.com>
2023-04-30 01:35:35 +0900

    User의 리스트를 반환하도록 변경

--- lib/ViewModel/unfollow_view_model.dart
+++ lib/ViewModel/unfollow_view_model.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/foundation.dart';
-import 'package:github_unfollow_checker/Model/user_list.dart';
+import 'package:github_unfollow_checker/Model/user.dart';
 import 'package:github_unfollow_checker/Repository/unfollow_repository.dart';
 
 class UnfollowViewModel with ChangeNotifier {
   final UnfollowRepository _repository = UnfollowRepository();
-  UserList _userList = UserList(user: List.empty(growable: true));
+  List<User> _userList = List.empty(growable: true);
 
-  UserList get userList => _userList;
+  List<User> get userList => _userList;
 
   Future getUserList(String queryName, userName) async {
     _userList = await _repository.getUserList(queryName, userName);
