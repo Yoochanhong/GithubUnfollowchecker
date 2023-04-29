c6e23434e7938e3b6b2b16c393aae1a8015a7354
yoochanhong <chanhongy6@naver.com>
2023-04-30 01:22:37 +0900

    User 객체의 리스트를 리턴하도록 변경

--- lib/DataSource/data_source.dart
+++ lib/DataSource/data_source.dart
@@ -1,13 +1,15 @@
-import 'package:github_unfollow_checker/Model/user_list.dart';
+import 'package:github_unfollow_checker/Model/user.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class DataSource {
-  Future<UserList> getUserList(String queryName, String userName) async {
+  Future<List<User>> getUserList(String queryName, String userName) async {
     final response = await http
         .get(Uri.parse("http://localhost:8080/$queryName?userName=$userName"));
     if (response.statusCode == 200) {
-      return UserList.fromJson(jsonDecode(response.body));
+      List<User> user = <User>[];
+      user = jsonDecode(response.body).map((i) => User.fromJson(i)).toList();
+      return user;
     }
     throw Exception("존재하지 않는 유저");
   }
