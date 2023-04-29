65f79907cd9bbec917d7b6a2f985443e11d679c0
yoochanhong <chanhongy6@naver.com>
2023-04-30 00:46:26 +0900

    유저 리스트를 받아올 메소드 구현

--- lib/DataSource/data_source.dart
+++ lib/DataSource/data_source.dart
@@ -1,3 +1,14 @@
-class DataSource{
-  
+import 'package:github_unfollow_checker/Model/user_list.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class DataSource {
+  Future<UserList> getUserList(String queryName, String userName) async {
+    final response = await http
+        .get(Uri.parse("http://localhost:8080/$queryName?userName=$userName"));
+    if (response.statusCode == 200) {
+      return UserList.fromJson(jsonDecode(response.body));
+    }
+    throw Exception("존재하지 않는 유저");
+  }
 }
