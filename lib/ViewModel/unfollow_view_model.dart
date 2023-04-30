69cd63b6105867a124fda70e0f087a9ce4ddfd69
yoochanhong <chanhongy6@naver.com>
2023-04-30 16:49:31 +0900

    검색 메소드 추가

--- lib/ViewModel/unfollow_view_model.dart
+++ lib/ViewModel/unfollow_view_model.dart
@@ -5,10 +5,11 @@ import 'package:github_unfollow_checker/Repository/unfollow_repository.dart';
 class UnfollowViewModel with ChangeNotifier {
   late final UnfollowRepository _repository;
   List<User> _userList = List.empty(growable: true);
+  List<User> _backupList = List.empty(growable: true);
 
   List<User> get userList => _userList;
 
-  UnfollowViewModel(){
+  UnfollowViewModel() {
     _repository = UnfollowRepository();
   }
 
@@ -16,6 +17,26 @@ class UnfollowViewModel with ChangeNotifier {
     _userList = List.empty();
     notifyListeners();
     _userList = await _repository.getUserList(queryName, userName);
+    _backupList = _userList;
+    notifyListeners();
+  }
+
+  void search(String query) {
+    List<User> result = List.empty(growable: true);
+    for (User user in _backupList) {
+      if (user.login!.contains(query)) {
+        result.add(user);
+      }
+    }
+    if (result.isEmpty) {
+      User user = User(
+          login: "검색결과가 존재하지 않습니다.",
+          htmlUrl: "https://github.com/asdfsdgdfgr",
+          avatarUrl:
+              "https://search.pstatic.net/sunny/?src=https%3A%2F%2Flive.staticflickr.com%2F3505%2F4070809932_21ff533bed.jpg&type=sc960_832");
+      result.add(user);
+    }
+    _userList = result;
     notifyListeners();
   }
 }
