915eaaee83c6833560904111923c3196eb14e9f0
yoochanhong <chanhongy6@naver.com>
2023-04-30 11:40:22 +0900

    getUserList로 유저 검색 후 적용

--- lib/View/login_page.dart
+++ lib/View/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:github_unfollow_checker/View/un_followers_check_page.dart';
 import 'package:github_unfollow_checker/View/un_following_check_page.dart';
+import 'package:github_unfollow_checker/ViewModel/unfollow_view_model.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -13,9 +15,11 @@ class _LoginPageState extends State<LoginPage> {
   final _formkey = GlobalKey<FormState>();
 
   TextEditingController controller = TextEditingController();
+  late UnfollowViewModel viewModel;
 
   @override
   Widget build(BuildContext context) {
+    viewModel = Provider.of(context);
     return Scaffold(
       backgroundColor: const Color(0xffFFFFFF),
       body: Column(
@@ -47,6 +51,7 @@ class _LoginPageState extends State<LoginPage> {
                   validator: (value) => value!.isEmpty ? '아이디를 입력해주세요.' : null,
                   onFieldSubmitted: (String str) {
                     if (_formkey.currentState!.validate()) {
+                      viewModel.getUserList("unfollowers", str);
                       Navigator.push(
                         context,
                         MaterialPageRoute(
@@ -65,6 +70,7 @@ class _LoginPageState extends State<LoginPage> {
               ElevatedButton(
                 onPressed: () {
                   if (_formkey.currentState!.validate()) {
+                    viewModel.getUserList("unfollowers", controller.text);
                     Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -78,6 +84,7 @@ class _LoginPageState extends State<LoginPage> {
               ElevatedButton(
                 onPressed: () {
                   if (_formkey.currentState!.validate()) {
+                    viewModel.getUserList("unfollowing", controller.text);
                     Navigator.push(
                       context,
                       MaterialPageRoute(
