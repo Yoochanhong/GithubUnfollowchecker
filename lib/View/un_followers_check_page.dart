1e2ec82524e4f4f63ac93c727e0283b268d1681d
yoochanhong <chanhongy6@naver.com>
2023-04-30 16:14:43 +0900

    로딩 인디케이터 구현

--- lib/View/un_followers_check_page.dart
+++ lib/View/un_followers_check_page.dart
@@ -13,38 +13,41 @@ class UnFollowCheckpage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xffFFFFFF),
       body: Center(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width - 50,
-          height: MediaQuery.of(context).size.height - 200,
-          child: ListView.builder(
-            itemCount: viewModel.userList.length,
-            itemBuilder: (context, index) {
-              return Container(
-                margin: const EdgeInsets.only(bottom: 13.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  border: Border.all(
-                    color: Colors.black,
-                  ),
-                ),
-                child: ListTile(
-                  leading: Image.network(
-                      viewModel.userList[index].avatarUrl.toString()),
-                  title: Text(viewModel.userList[index].login.toString()),
-                  onLongPress: () {
-                    String url = viewModel.userList[index].htmlUrl.toString();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => WebViewScreen(url: url),
+        child: viewModel.userList.isNotEmpty
+            ? SizedBox(
+                width: MediaQuery.of(context).size.width - 50,
+                height: MediaQuery.of(context).size.height - 200,
+                child: ListView.builder(
+                  itemCount: viewModel.userList.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 13.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(
+                          color: Colors.black,
+                        ),
+                      ),
+                      child: ListTile(
+                        leading: Image.network(
+                            viewModel.userList[index].avatarUrl.toString()),
+                        title: Text(viewModel.userList[index].login.toString()),
+                        onLongPress: () {
+                          String url =
+                              viewModel.userList[index].htmlUrl.toString();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => WebViewScreen(url: url),
+                            ),
+                          );
+                        },
                       ),
                     );
                   },
                 ),
-              );
-            },
-          ),
-        ),
+              )
+            : const CircularProgressIndicator(),
       ),
     );
   }
